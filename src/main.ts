5f04015bc86a686e32768485f105faa4a4b123c7
StefanoBichicchi <bichicchi.stefano@gmail.com>
2021-09-29 11:52:22 +0200

    add config service

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const logger = new Logger();
+  const logger = new Logger('Main App');
 
   const app = await NestFactory.create(AppModule);
 
